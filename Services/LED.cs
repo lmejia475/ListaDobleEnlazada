a0963d78f725e4c3b5a943dd7fd3060c464138e1
luis mejia <luis03mejia03@gmail.com>
2025-04-03 00:19:15 -0600

    Se añade funcionalidad de Eliminar al Final y Eliminar Antes de X

--- Services/LED.cs
+++ Services/LED.cs
@@ -16,5 +16,105 @@ namespace ListaEnlazadaDoble.Services
         {
             return PrimerNodo == null;
         }
+
+        public string AgregarAlFinal(Nodo nodo) {
+            
+            if(EstaVacia())
+            {
+                PrimerNodo = nodo;
+                UltimoNodo = nodo;
+                return "Nodo agregado con éxito";
+
+            }
+            else
+            {
+                UltimoNodo.ReferenciaSiguiente = nodo;
+                nodo.ReferenciaAnterior = UltimoNodo;
+                UltimoNodo = nodo;
+                return "Nodo agregado con éxito";
+            }
+            return "Error";
+        }
+        
+        public string EliminarAntesDeX(string dato) { 
+            Nodo aux = PrimerNodo;
+
+            if (EstaVacia())
+            {
+                return "Lista vacía";
+            }
+            if (PrimerNodo.Informacion == dato)
+            {
+                return ("No hay nodos por eliminar antes del dato: " + dato);
+            }
+            while (aux != null)
+            {
+
+                if (aux.Informacion == dato && aux.ReferenciaAnterior == PrimerNodo && aux == UltimoNodo)
+                {
+                    aux.ReferenciaAnterior = null;
+                    aux.ReferenciaSiguiente = null;
+                    PrimerNodo = aux;
+                    UltimoNodo = aux;
+                    return "Nodo eliminado con éxito";
+                }
+
+
+                if (aux.Informacion == dato && aux.ReferenciaAnterior == PrimerNodo && aux != UltimoNodo)
+                {
+                    aux.ReferenciaAnterior = null;
+                    PrimerNodo = aux;
+                    return "Nodo eliminado con éxito";
+                }
+
+
+                if (aux.Informacion == dato && aux == UltimoNodo)
+                {
+                    aux.ReferenciaAnterior.ReferenciaAnterior.ReferenciaSiguiente = UltimoNodo;
+                    UltimoNodo.ReferenciaAnterior = aux.ReferenciaAnterior.ReferenciaAnterior;
+                    aux = null;
+                    return "Nodo eliminado con éxito";
+                }
+
+                aux = aux.ReferenciaSiguiente;
+            }
+                
+            return "Error al eliminar nodo";
+        }
+
+
+        public string EliminarAlFinal()
+        {
+            Nodo aux = PrimerNodo;
+
+            if (EstaVacia())
+            {
+                return "Lista vacía";
+            }
+            if (PrimerNodo == UltimoNodo)
+            {
+                PrimerNodo = null;
+                UltimoNodo = null;
+                return ("Se eliminó el ultimo Nodo");
+            }
+
+            while (aux != null)
+            {
+             
+                if(aux.ReferenciaSiguiente == UltimoNodo)
+                {
+                    aux.ReferenciaSiguiente = null;
+                    UltimoNodo = aux;
+                    return "Nodo eliminado con éxito";
+                }
+                aux = aux.ReferenciaSiguiente;
+            }
+
+            return "Error al eliminar nodo";
+        }
+
+
+
+
     }
 }
