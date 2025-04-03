7cb1b666f6db234dafeb4cee783be3a47290d7a6
luis mejia <luis03mejia03@gmail.com>
2025-04-02 22:39:13 -0600

    Se añaden modelos y services

---
+++ Models/Nodo.cs
@@ -0,0 +1,29 @@
+﻿namespace ListaEnlazadaDoble.Models
+{
+    public class Nodo
+    {
+        public string Informacion { get; set; }
+        public Nodo? ReferenciaAnterior { get; set; }
+        public Nodo? ReferenciaSiguiente { get; set; }
+
+        public Nodo()
+        {
+            Informacion = string.Empty;
+            ReferenciaAnterior = null;
+            ReferenciaSiguiente = null;
+        }
+
+        public Nodo(string informacion)
+        {
+            Informacion = informacion;
+            ReferenciaAnterior = null;
+            ReferenciaSiguiente = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion} ==> ";
+        }
+    }
+}
+
---
+++ Services/LED.cs
@@ -0,0 +1,20 @@
+﻿using ListaEnlazadaDoble.Models;
+
+namespace ListaEnlazadaDoble.Services
+{
+    public class LED
+    {
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
+
+        public LED()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+        }
+        bool EstaVacia()
+        {
+            return PrimerNodo == null;
+        }
+    }
+}
